6844b04504cee798c34bf4b577b05b7ca216cbb3
Ojas Joshi <146631578+Joshiojas185@users.noreply.github.com>
2025-04-01 11:17:43 +0530

    Update script.js

--- host/script.js
+++ host/script.js
@@ -36,17 +36,21 @@ function getQueryParams() {
 }
 
 window.onload = () => {
-    const params = getQueryParams();
-    if (params.room) {
-        const playerName = params.name;
-        roomInput.value = params.room;
-        nameInput.value = playerName;
 
-        // Delay the join event to ensure the socket connection is established
-        setTimeout(() => {
-            joinBtn.click(); // Simulate click to join the room
-        }, 100); // Adjust the delay as needed (100ms is a good starting point)
-    }   
+    setTimeout(() => {
+        joinLive('tcs','tcshost')
+    }, 100);
+    // const params = getQueryParams();
+    // if (params.room) {
+    //     const playerName = params.name;
+    //     roomInput.value = params.room;
+    //     nameInput.value = playerName;
+
+    //     // Delay the join event to ensure the socket connection is established
+    //     setTimeout(() => {
+    //         joinBtn.click(); // Simulate click to join the room
+    //     }, 100); // Adjust the delay as needed (100ms is a good starting point)
+    // }   
 };
 
 
